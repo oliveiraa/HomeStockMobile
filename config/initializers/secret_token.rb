6e17c6c780ce6979807003812b0a753bc4261d6f
Alvaro Oliveira <alvaro.oliveira@gmail.com>
2012-03-07 21:29:24 -0300

    Rails app created

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+HomeStockMobile::Application.config.secret_token = '4bfe3a95c59cc1661a3f19f103dce00c82bd24167010e74cdb5a6188d0528ae15f48ebbd416a2f6893c979ada5dae01ab706960878998ef4f5f5124d9cb50cf4'
